4aa7b19317973dfcb88d790b43ce90c7e8af36ce
jaeho <jaeho7423@gamil.com>
2024-09-29 18:55:42 +0900

    threadManager 구현

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -1,46 +1,27 @@
 ﻿#include "pch.h"
 #include <iostream>
 #include "CorePch.h"
-#include <thread>
+#include "AccountManager.h"
+#include "UserManager.h"
+#include <windows.h>
 #include <atomic>
-#include <mutex>
-#include <Windows.h>
-#include <future>
-#include "ConcurrentQueue.h"
-#include "ConcurrentStack.h"
+#include "ThreadManager.h"
+#include "GameServer.h"
 
-using namespace std;
+CoreGlobal core;
 
-//LockQueue<int32> q;
-LockFreeStack<int32> s;
-
-void push() {
+void ThreadMain() {
 	while (true) {
-		int32 value = rand() % 100;
-		s.Push(value);
-
-		this_thread::sleep_for(10ms);
+		cout << "Hello! I am thread ..." << LThreadId << endl;
+		this_thread::sleep_for(1s);
 	}
 }
-
-void pop() {
-	while (true) {
-		 int32 value;
-		 if (s.TryPop(OUT value)) {
-			 cout << value << endl;
-
-		 }
-	}
-}
-
 
 int main() {
-	thread t1(push);
-	thread t2(pop);
-	//thread t3(pop);
 
-	t1.join();
-	t2.join();
-	//t3.join();
+	for (int32 i = 0; i < 5; i++) {
+		GThreadManager->Launch(ThreadMain);
+	}
+	GThreadManager->Join();
+
 }
-	
---
+++ Server/ServerCore/Lock.h
@@ -0,0 +1,10 @@
+#pragma once
+
+
+// 표준 mutex 문제점
+// 1. 재귀적으로 lock을 못 잡음. (물론 recursive_lock 자료형이 따로 있긴 함.)
+// 2. 
+class Lock
+{
+};
+
