3b467aec2c290810c845395a1d02e66086a7ef0e
ChoiJaeho180 <jaeho7423@gmail.com>
2025-01-05 14:18:00 +0900

    stompAllocator
    경계 초과(overflow 위주로 잡음), 해제 후 접근 등의 문제를 조기에 감지

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -12,7 +12,12 @@
 #include "RefCounting.h"
 #include "Memory.h"
 
-class Knight {
+
+class Player {
+
+};
+
+class Knight: public Player {
 public:
 	Knight() { cout << "Knight()" << endl; };
 	Knight(int32 hp) : _hp(hp) {
@@ -31,14 +36,36 @@ public:
 	//	::free(ptr);
 
 	//}
-private:
+public:
 	int _hp;
 };
 
-
-
-
 int main() {
-	Knight* knight = xnew<Knight>(100);
-	xdelete(knight);
+	/*
+	* a 객체를 삭제한 이후에도 접근이 가능한 이유
+	* -> 메모리 해제 요청했을 때, 운영체제에 즉시 반환하는 것이 아니라 
+	* -> "힙 관리자가 관리하는 메모리 풀에 반환될 가능성이 높음."
+	*	-> 힙 관리자는 성능을 높이기 위해 메모리를 즉시 운영 체제에 반환하지 않고, 
+	*	-> 다음 요청에 제사용할 수 있도록 내부적으로 관리
+	* 정리
+	* -> delete 후에도 해당 메모리 주소에 접근하면 여전히 이전 값이 남아 있을 수 있음.
+	* 
+	* 개선점
+	* -> virtualFree 사용 (use-after-free 해소)
+	*	-> 메모리 반환 시 즉시 반환되어, 해당 메모리 접근 시 즉각적인 충돌이 발생할 가능성이 높음..
+	*/
+	Knight* a = new Knight();
+	a->_hp = 100;
+	delete a;
+	a->_hp = 200;
+
+	// 해제 후 접근 개선(내부에 virtualMalloc, virtualFree 사용)
+	Knight* knight = xnew<Knight>();
+	xdelete<Knight>(knight);
+	knight->_hp = 100;
+
+	// 경계 초과 테스트
+	Knight* test = (Knight*)xnew<Player>();
+	test->_hp = 100;
+	xdelete(test);
 }
--- Server/ServerCore/Allocator.h
+++ Server/ServerCore/Allocator.h
@@ -11,3 +11,15 @@ public:
 	static void		Release(void* ptr);
 };
 
+/*
+	stompAllocator : 경계 초과(overflow 위주로 잡음), 해제 후 접근 등의 문제를 조기에 감지. 
+	
+*/
+class StompAllocator
+{
+	enum { PAGE_SIZE = 0x1000 };
+public:
+	static void*		Alloc(int32 size);
+	static void			Release(void* ptr);
+};
+
