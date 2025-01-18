1cd8051a5d42c12aaa6def78ca33eed4e53d46d5
jaeho <jaeho7423@gamil.com>
2025-01-18 11:46:37 +0900

    멀티 스레드 환경에서 사용 가능한 SListEntry 구현

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -12,45 +12,55 @@
 #include "RefCounting.h"
 #include "Memory.h"
 #include "Allocator.h"
+#include "LockFreeStack.h"
 
-class Player {
+DECLSPEC_ALIGN(16)
+class Data {
+public:
+	SListEntry _entry;
+	int64 _rand;
 
+	Data() : _rand(rand() % 100) {}
+	~Data() {};
 };
 
-class Knight: public Player {
-public:
-	Knight() { cout << "Knight()" << endl; };
-	Knight(int32 hp) : _hp(hp) {
-		cout << "Knight(hp)" << endl;
-	}
-	~Knight() { cout << "~Knight()" << endl; };
-
-	//void* operator new(size_t size) {
-	//	cout << "Knight new! " << size << endl;
-	//	void* ptr = ::malloc(size);
-	//	return ptr;
-	//}
-
-	//void operator delete(void* ptr) {
-	//	cout << "Knight delete!" << endl;
-	//	::free(ptr);
-
-	//}
-public:
-	int _hp;
-};
+SListHeader* GHeader;
 
 int main() {
-	//for (int32 i = 0; i < 5; i++) {
-	//	GThreadManager->Launch([]() {
-	//		while (true) {
-	//			Vector<Knight> v(10);
-	//			/*Map<int32, Knight> m;
-	//			m[100] = Knight();*/
-	//			this_thread::sleep_for(10ms);
-	//		}
-	//	});
+	GHeader = new SListHeader();
+	ASSERT_CRASH(((uint64)GHeader % 16) == 0);
+	InitalizeHead(GHeader);
 
-	//}
-	//GThreadManager->Join();
+
+	for (int32 i = 0; i < 3; i++) {
+		GThreadManager->Launch([]() 
+			{
+				while (true) {
+					Data* data = new Data();
+					ASSERT_CRASH(((uint64)data % 16) == 0);
+
+					PushEntrySList(GHeader, (SListEntry*)data);
+					this_thread::sleep_for(10ms);
+				}
+			});
+	}
+
+	for (int32 i = 0; i < 2; i++) {
+		GThreadManager->Launch([]()
+			{
+				while (true) {
+					Data* pop = nullptr;
+					pop = (Data*)PopEntrySList(GHeader);
+					if (pop) {
+						cout << pop->_rand << endl;
+						delete pop;
+					}
+					else {
+						cout << "NONE" << endl;
+					}
+				}
+			});
+	}
+
+	GThreadManager->Join();
 }
