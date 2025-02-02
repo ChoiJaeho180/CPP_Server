efbceb8f7162a6c21b6ec0d8b5808b839599a198
jaeho <jaeho7423@gamil.com>
2025-02-02 16:05:52 +0900

    static_cast를 이용한 dynamic_cast 구현

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -14,42 +14,81 @@
 #include "Allocator.h"
 #include "LockFreeStack.h"
 
-class Knight {
+using TL = TypeList<class Player, class Mage, class Knight, class Archer>;
+class Player {
+public:
+	Player() {
+		INIT_TL(Player);
+	}
+	virtual ~Player() {}
+
+	DECLARE_TL;
+};
+
+class Knight: public Player {
 public:
 	int32 _hp = rand() % 1000;
+	Knight() {
+		INIT_TL(Knight);
+	}
+
 };
 
-class Monster {
+class Mage : public Player {
 public:
-	int64 _id = 0;
+	Mage() {
+		INIT_TL(Mage);
+	}
 };
+
+class Archer : public Player {
+public:
+
+	Archer() {
+		INIT_TL(Archer);
+	}
+};
+
 
 int main() { 
+	Player* p1 = new Knight();
+	Player* p2 = new Mage();
 
-	Knight* knights[100];
-	for (int32 i = 0; i < 100; i++) {
-		Knight* k = ObjectPool<Knight>::Pop();
-		knights[i] = k;
+	Knight* k1 = dynamic_cast<Knight*>(p1);
+	// static_cast
+	//	-> mage 객체도 knight로 형 변환이 가능하여 메모리 오염이 생길 수 있음
+	// dynamic_cast
+	//  -> 형 변환이 되지 않을 경우 nullptr를 반환 
+	//  -> 속도가 느림
+
+
+	TypeList<Mage, Knight>::Head whoAmI;
+	TypeList<Mage, Knight>::Tail whoAmTail;
+	
+	int32 len1 = Length<TypeList<Mage, Knight, Archer>>::value;
+
+	bool canConvert = Conversion<Player, Knight>::exists;
+	bool canConvert2 = Conversion<Knight, Player>::exists;
+
+	TypeConversion<TL> test;
+	auto a = test.s_convert[0];
+	auto b = test.s_convert[1];
+	{
+		Player* player = new Player();
+		bool canCast = CanCast<Knight*>(player);
+		Knight* knight = TypeCast<Knight*>(player);
+		delete player;
 	}
 
-	for (int32 i = 0; i < 100; i++) {
-		ObjectPool<Knight>::Push(knights[i]);
-		knights[i] = nullptr;
+	{
+		shared_ptr<Knight> knight = MakeShared<Knight>();
+		shared_ptr<Player> player = TypeCast<Player>(knight);
 	}
-
-	shared_ptr<Knight> ptr = ObjectPool<Knight>::MakeShared();
-	shared_ptr<Knight> ptr2 = MakeShared<Knight>();
-
 
 	/*for (int32 i = 0; i < 3; i++) {
 		GThreadManager->Launch([]() 
 			{
 				while (true) {
-					Knight* knight = xnew<Knight>();
-					cout << knight->_hp << endl;
-					this_thread::sleep_for(10ms);
-
-					xdelete(knight);
 				}
 			});
 	}
